0cbb92ba172f332f80f151954d29c9bf4060bec4
OscarFabiani <spartan539@yahoo.com>
2019-05-14 20:39:42 -0700

    Updates

---
+++ precache-manifest.04fc20d66fb5d8877d8d0eedb46160da.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/react-nutrition-calculator/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "27c9e09e3cafc23d3d10",
+    "url": "/react-nutrition-calculator/static/css/main.2f43c43f.chunk.css"
+  },
+  {
+    "revision": "54fbbb6a59c59724b20d",
+    "url": "/react-nutrition-calculator/static/js/runtime~main.d5b9e548.js"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/react-nutrition-calculator/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "4d29ca42162e55d6e5eb",
+    "url": "/react-nutrition-calculator/static/js/2.107d755d.chunk.js"
+  },
+  {
+    "revision": "27c9e09e3cafc23d3d10",
+    "url": "/react-nutrition-calculator/static/js/main.bbc98bf2.chunk.js"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/react-nutrition-calculator/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/react-nutrition-calculator/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/react-nutrition-calculator/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "4d29ca42162e55d6e5eb",
+    "url": "/react-nutrition-calculator/static/css/2.f4e987ed.chunk.css"
+  },
+  {
+    "revision": "99c743ba349fdf324fea06475f5b7b75",
+    "url": "/react-nutrition-calculator/index.html"
+  }
+];
